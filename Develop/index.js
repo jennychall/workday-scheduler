a1ae260732ad9156536212d1ff33d1dde39d1e39
Jenny Hall <jenny hall>
2022-07-18 21:47:46 -0700

    Finished time intervals

--- Develop/index.js
+++ Develop/index.js
@@ -8,4 +8,33 @@ var tenAm = $("10am");
 var elevenAm = $("11am");
 var twelveAM = $("12pm");
 var onePm = $("13pm");
+var twoPm = $("14pm");
+var threePm = $("15pm");
+var fourPm = $("16pm");
+var fivePm = $("17pm");
+var sixPm = $("18pm");
+var sevenPm = $("19pm");
 
+var hour = moment().hours();
+var userInput;
+var hourSpan; 
+//variable hourString = $(".hour").text().split(" ");
+
+//date and hour
+
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
+  
+  function initPage() {
+
+
+
+
+
+    
+  }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # workday-scheduler
+
+05 Third-Party API's: Work Day Scheduler 
+
+## Description 
+
+A calender application that allows for a use to save their events on an hourly basis per day. This app uses HTML and CSS via jQuery. 
+This app is using API moment.js. 
+
