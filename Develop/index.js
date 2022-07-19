d6603e7c45381cbdcf792fe6853148d8da7637ec
Jenny Hall <jenny hall>
2022-07-18 21:20:21 -0700

    added moment.js to js file and partial time variables

---
+++ Develop/index.js
@@ -0,0 +1,11 @@
+//added moment.js
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
+
+//Hours
+var nineAm = $("#9am");
+var tenAm = $("10am");
+var elevenAm = $("11am");
+var twelveAM = $("12pm");
+var onePm = $("13pm");
+
